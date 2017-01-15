ccd0728e1bb0638d2d2bda45f8498cab9cc0898a
Noah UC <noah@gus.ucsb.edu>
2017-01-14 21:38:35 -0800

    Test drive of the integer-based linked list

--- src/com/feralo/Main.java
+++ src/com/feralo/Main.java
@@ -3,6 +3,9 @@ package com.feralo;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // Test drive of the integer-based linked list
+        IntNode node = new IntNode(12, null);
+        node.addNodeAfter(155);
+        System.out.println(IntNode.listLength(node));
     }
 }
