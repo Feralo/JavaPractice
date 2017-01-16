c725311572e07c1815fffb8e4a143d3e545f60c7
Noah UC <noah@gus.ucsb.edu>
2017-01-16 07:54:49 -0800

    Fizz buzz challenge

--- src/com/feralo/Main.java
+++ src/com/feralo/Main.java
@@ -3,9 +3,19 @@ package com.feralo;
 public class Main {
 
     public static void main(String[] args) {
-        // Test drive of the integer-based linked list
+        System.out.println("Test drive of the integer-based linked list");
+
         IntNode node = new IntNode(12, null);
         node.addNodeAfter(155);
         System.out.println(IntNode.listLength(node));
+
+        System.out.println("Fizz-Buzz test");
+        System.out.println(IntNode.listLength(node));
+        for (int i = 1; i <= 20; i++){
+            if (i % (3*5) == 0)  System.out.println("FizzBuzz");
+            else if (i % 5 == 0)  System.out.println("Buzz");
+            else if (i % 3 == 0)  System.out.println("Fizz");
+            else System.out.println(i);
+        }
     }
 }
