871699cc94a78a3eeda37be0c7a2ecb8f1e7c0ee
Noah UC <noah@gus.ucsb.edu>
2017-01-16 07:21:35 -0800

    Procedural Fibonacci calculator

---
+++ src/com/feralo/FibonacciCalculator.java
@@ -0,0 +1,35 @@
+package com.feralo;
+
+import java.util.Scanner;
+
+
+public class FibonacciCalculator {
+
+    public static void main(String args[]) {
+
+        // Input to print Fibonacci series
+        System.out.println("Enter the number of Fibonacci numbers to print");
+        int number = new Scanner(System.in).nextInt();
+
+        System.out.println("First " + number +" members of the Fibonaci series:");
+        // Print the series
+        for(int i=1; i<=number; i++)
+            System.out.print(fibonaci2(i)+ " ");
+    }
+
+    private static int fibonaci2(int number){
+        // handle the 'base case'
+        if(number == 1 || number == 2){
+            return 1;
+        }
+
+        // build up the string to do all the work
+        int fibo1=1, fibo2=1, fibonacci=1;
+        for(int i = 3; i <= number; i++){
+           fibonacci = fibo1 + fibo2;
+           fibo1 = fibo2;
+           fibo2 = fibonacci;
+        }
+        return fibonacci;
+    }
+}
